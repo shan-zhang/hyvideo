030a3bb694c1e2f1cf30ff6bc2be19f79d008c84
mengxj08 <mengxj08@gmail.com>
2016-09-07 21:52:43 +0800

    Add quiz for the navigation study

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
     $showVideo = "practice";
     //$showVideo = "video1";
-    //$showVideo = "video3";
+    //$showVideo = "video2";
 
     $videoName = 'video/video1.mp4';
     $videoSubtitle = 'video/src/video1.vtt';
@@ -74,19 +74,17 @@
             <!-- <button id="conceptsMapping" onclick="clickConceptMap()">Auto-Concepts: off</button> -->
             <h3 id="clips"></h3>
             <!-- The code below is for quiz -->
-            <!-- <button id="startQuiz" onclick="startQuiz(event)">Start Quiz!</button> -->
-    <!--         <form action="php/grade.php" method="post" id="myForm" style="display:none">
-                  <label id='quizLabel'></label><br>
-                  <label id='quizContent'></label><br><br>
-                  <div id='quizRadio' style="display:none">
-                      <input type="radio" value="true" name="answer">True<br>
-                      <input type="radio" value="false" name="answer">False<br><br>
-                      <input type="submit" name="submit" value="submit">
-                  </div>
+            <button id="startQuiz" onclick="startQuiz(event)">Start Quiz!</button>
+            <br />
+            <br />
+            <form action="php/grade.php" method="post" id="myForm" style="display:none">
+                  <label id='quizLabel'></label><br /><br />
+                  <label id='quizContent'></label><br /><br />
+                  <input type="submit" name="submit" value="submit">
             </form>
-            <button id='radioOption' onclick="showRadio(event)" style="display:none">Ready!</button>
+            <!-- <button id='radioOption' onclick="showRadio(event)" style="display:none">Ready!</button> -->
             <br/>
-            <h3 id="closingQuiz" style="display:none">The quiz is over. Thanks for participating. <a href='download.php' onclick="downloadResult()">Download</a>the study result.</h3> -->
+            <h3 id="closingQuiz" style="display:none">The quiz is over. Thanks for participating. <a href='download.php' onclick="downloadResult()">Download</a>the study result.</h3>
 
             <div id='footerButton'>
 <!--                 <label>Suggested Key Concepts:</label>
@@ -658,64 +656,49 @@
             });
         }
         var quizNum = 0;
-        var quizTask = {'pre': "<?php echo 'pre-'.$showVideo; ?>",'post': "<?php echo 'post-'.$showVideo; ?>" };
-        var quizType = 'pre';
+        var quizTask = {'post': "<?php echo 'post-'.$showVideo; ?>" };
+        var quizType = 'post';
         var quizTime;
         function startQuiz(e){
             $("#footerButton").css("display","none");
             $("#startQuiz").css("display","none");
             setForm();
             $("#myForm").css("display","inline");
-            $('#radioOption').css("display","inline");
+            quizTime = e.timeStamp;
         }
 
         function setForm() {
             if(quizNum == quiz[quizTask[quizType]].length)
                 return false;
             else{
-                $('#quizLabel').text(quiz[quizTask[quizType]][quizNum]['title']);
-                $('#quizContent').text(quiz[quizTask[quizType]][quizNum]['content']);
-                $('#quizRadio').css("display","none");
-                $('#radioOption').css("display","inline");
+                $('#quizLabel').html(quiz[quizTask[quizType]][quizNum]['title']);
+                $('#quizContent').html(quiz[quizTask[quizType]][quizNum]['content']);
                 return true;
             }
 
         }
 
-        function showRadio(e){
-            quizTime = e.timeStamp;
-            $('#radioOption').css("display","none");
-            $('#quizRadio').css("display","inline");
-        }
-
         function downloadResult(){
             console.log("Downloading results...");
-            if(quizType == 'pre'){
-                quizType = 'post';
-                quizNum = 0;
-                $("#closingQuiz").css("display","none");
-                $("#startQuiz").css("display","inline");
-            }
         }
 
         $('#myForm').submit(function(e){
             e.preventDefault();
             var timePerQuiz = e.timeStamp - quizTime;
-            var data = $("#myForm").serialize() + "&quiz=" + quizNum + "&time=" + timePerQuiz + "&quizType=" + quizType;
+            var data = "&quiz=" + (quizNum + 1) + "&time=" + timePerQuiz + "&quizType=" + quizType;
+            quizTime = e.timeStamp;
             $.ajax({
                 url:$("#myForm").attr("action"),
                 type:'post',
                 data: data,
                 success:function(response){
                     quizNum ++;
-                    $("input:radio").removeAttr("checked");
+                    //$("input:radio").removeAttr("checked");
                     if(!setForm()){
                         $("#myForm").css("display","none");
                         $("#closingQuiz").css("display","inline");
                     }
                     console.log(response);
-                    //console.log($("#myForm").serialize());
-                    //whatever you wanna do after the form is successfully submitted
                 }
             });
         });
